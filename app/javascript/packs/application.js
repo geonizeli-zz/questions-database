eff511a04a304f2ec2f33a06d3ff028cf82237fb
João Victor <jvsouzax@gmail.com>
2020-02-20 20:49:14 -0300

    add: action_text

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,4 +2,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("trix")
+require("@rails/actiontext")
 //= require materialize
