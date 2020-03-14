9e0488eca9012a4181e95dd735af380babfd3c03
João Victor <jvsouzax@gmail.com>
2020-03-14 15:24:20 -0300

    add: Course seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 User.create!(email: 'example@admin.com',
              password: 'password',
              password_confirmation: 'password',
              name: 'Admin',
-             admin: true)
+             admin: true) if User.where(email: 'example@admin.com').empty?
+
+adm = Course.find_or_create_by(name: 'Administração')
+arq = Course.find_or_create_by(name: 'Arquitetura e Urbanismo')
+bio = Course.find_or_create_by(name: 'Biomediciona')
+cer = Course.find_or_create_by(name: 'Cervejaria')
+cop = Course.find_or_create_by(name: 'Ciência da Computação')
+bi1 = Course.find_or_create_by(name: 'Ciências Biológicas (Bacharelado')
+bi2 = Course.find_or_create_by(name: 'Ciências Biológicas (Licenciatura)')
+con = Course.find_or_create_by(name: 'Ciências Contábeis')
+dir = Course.find_or_create_by(name: 'Direito')
+enf = Course.find_or_create_by(name: 'Enfermagem')
+civ = Course.find_or_create_by(name: 'Eng. Civil')
+pro = Course.find_or_create_by(name: 'Eng. Produção')
+far = Course.find_or_create_by(name: 'Farmárcia')
+fis = Course.find_or_create_by(name: 'Fisioterapia')
+jdi = Course.find_or_create_by(name: 'Jogos Digitais')
+jor = Course.find_or_create_by(name: 'Jornalismo')
+med = Course.find_or_create_by(name: 'Medicina')
+vet = Course.find_or_create_by(name: 'Medicina Veterinária')
+nut = Course.find_or_create_by(name: 'Nutrição')
+odo = Course.find_or_create_by(name: 'Odontologia')
+psi = Course.find_or_create_by(name: 'Psicologia')
+pub = Course.find_or_create_by(name: 'Publicidade e Propaganda')
