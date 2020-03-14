54c680122a71fdaae4892e9d94b669baec6bf9af
João Victor <jvsouzax@gmail.com>
2020-03-14 19:06:06 -0300

    remove: 'cancel my account' button

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
   root to: 'questions#index'
 
-  devise_for :users, controllers: { registrations: "registrations" }
+  devise_for :users, skip: :registrations
+  devise_scope :user do
+    resource :registration,
+      only: [:new, :create, :edit, :update],
+      path: 'users',
+      path_names: { new: 'sign_up' },
+      controller: 'registrations',
+      as: :user_registration do
+        get :cancel
+      end
+  end
 
   resources :courses, except: [:index]
   resources :subjects, except: [:index]
